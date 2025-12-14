f4e18d400bd30c8742434f59be3258d9a448d661
lienta3 <lien.ta3@gmail.com>
2025-12-14 19:22:26 +1100

    added moment plugin & js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
+function updateTime() {
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
 
+  tokyoDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("H:mm:ss [<small>]A[</small>]");
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("H:mm:ss [<small>]A[</small>]");
+}
+
+setInterval(updateTime, 1000);
